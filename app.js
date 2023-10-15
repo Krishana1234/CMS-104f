6d084e78e36aafa2b65232e67d9c9e9397264a85
Krishana1234 <146236815+Krishana1234@users.noreply.github.com>
2023-10-15 22:51:31 +0545

    add code

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const app = require("express")();
+//const app = require("express")();
 
 const { connectDatabase } = require("./database/database");
+const express = require("express");
+const Blog = require("./model/blogModel");
+const app = express();
+// nodejs lai form bata aako data parse gar vaneko ho 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
 
 //database connection
 connectDatabase()
@@ -13,8 +21,101 @@ app.get("/",(req,res)=>{
         message:"success"
     })
 })
+    // GET API => /blogs (All blogs)
+app.get("/blogs",async (req,res)=>{
+    // fetching/reading all Blogs from Blog model
+   const blogs =  await Blog.find()
+   // check if blogs contains data or not
+   if(blogs.length == 0){
+    res.status(404).json({
+        // status : 404,
+        message : "Empty blogs"
+    })
+   }else{
+       res.status(200).json({
+        
+           message : "Blogs fetched successfully",
+           blogs : blogs
+        })
+    }
+})
 
+// GET API -> /blogs/:id (single Blog)
+app.get("/blogs/:id",async (req,res)=>{
+   const id = req.params.id
 
+    const blog = await Blog.findById(id)
+    if(blog){
+        res.status(200).json({
+            message : "Blog fetched succesfully",
+            data : blog
+        })
+    }else{
+        res.status(404).json({
+            message : "No blog found"
+        })
+    }
+
+})
+
+// CREATE BLOG API  
+app.post("/blogs", async(req,res)=>{
+    const title = req.body.title;
+    const subTitle = req.body.subTitle
+    const description = req.body.description
+ 
+ //    Alternative (object destructuring)
+ //    const {title,subTitle,description} = req.body
+ 
+     // Insert to database logic goes here 
+    await Blog.create({
+         title : title  ,
+         subTitle : subTitle,
+         description : description
+     })
+     res.status(200).json({
+        message: "created sucessfully"
+     })
+    })
+     // UPDATE BLOG API 
+app.patch("/blogs/:id",async (req,res)=>{
+    const id = req.params.id
+    const title = req.body.title
+    const subTitle = req.body.subTitle
+    const description = req.body.description
+    
+    // const {title,subTitle,description} = req.body  ALTERNATIVE
+    
+
+    await Blog.findByIdAndUpdate(id,{
+        title : title,
+        subTitle : subTitle,
+        description : description
+    })
+
+    res.status(200).json({
+        message : "Blog updated Succesfully"
+    })
+
+})
+// DELETE API 
+app.delete("/blogs/:id",async (req,res)=>{
+    const id = req.params.id
+    // const {id} = req.params 
+
+    await Blog.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message : "Blog Deleted Successfully"
+    })
+})
+     
+ 
+    
+     // Alternative 
+     // res.status(200).json({
+     //     message : "Blog created successfully"
+     // })
 
 
 app.listen(2000,()=>{
--- database/database.js
+++ database/database.js
@@ -4,7 +4,7 @@ exports.connectDatabase = async ()=> {
     //connect database
 
     await mongoose.connect("mongodb+srv://hello:hello@cluster0.ldhl8f2.mongodb.net/?retryWrites=true&w=majority")
-     console.log("Database is connected sucessfully")
+     console. log("Database is connected sucessfully")
   
 
 }
